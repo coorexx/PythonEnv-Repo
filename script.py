0ab605d932e0523860bf61cd83c843d7d84463fb
Coco Acha <coorexx@gmail.com>
2020-04-30 23:27:58 +0300

    Removed some code

--- script.py
+++ script.py
@@ -1,20 +1,6 @@
-import sys
 import requests
-from os import rename
-import math
 
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Corey'))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
